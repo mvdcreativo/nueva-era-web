5259e4e684b9bfcda493ebc86bb71e86a0a88466
Emir Méndez <mvdcreativo@gmail.com>
2020-02-29 00:05:23 -0300

    cambio url environment api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API : "http://api.nuevaerauruguay.tk/api/",
+  API : "https://api.nuevaerauruguay.tk/api/",
   urlImg : "https://apiv2.jh.nuevaerauruguay.com/Upload/Article/",
-  AuthAPI: 'http://api.nuevaerauruguay.tk/api/auth/',
-  urlFiles: 'http://api.nuevaerauruguay.tk/storage/'
+  AuthAPI: 'https://api.nuevaerauruguay.tk/api/auth/',
+  urlFiles: 'https://api.nuevaerauruguay.tk/storage/'
 };
