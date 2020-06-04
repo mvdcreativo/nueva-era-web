99a076f7627fda441b1f0ae1b22566e981f24ad4
unknown <mvdcreativo@gmail.com>
2020-06-04 02:09:53 -0300

    imagenes dia de la madre

--- src/app/components/shop/home-four/home-four.component.ts
+++ src/app/components/shop/home-four/home-four.component.ts
@@ -33,10 +33,10 @@ export class HomeFourComponent implements OnInit {
 
 
   public slides = [
-    { title: '', subtitle: '', image: 'assets/images/carousel/ciberlunes1.jpeg' },
-    { title: '', subtitle: '', image: 'assets/images/carousel/ciberlunes.jpeg' },
+    { title: '', subtitle: '', image: 'assets/images/carousel/diamadre1.jpeg' },
+    { title: '', subtitle: '', image: 'assets/images/carousel/diamadre.jpeg' },
+    { title: '', subtitle: '', image: 'assets/images/carousel/envio-express.jpeg' },
     { title: '', subtitle: '', image: 'assets/images/carousel/envio.jpeg' },
-    // { title: '', subtitle: '', image: 'assets/images/carousel/3050639.jpg' },
     // { title: 'Massive sale', subtitle: 'Only for today', image: 'assets/images/carousel/banner5.jpg' }
   ];
 
