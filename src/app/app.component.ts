e635ff6824f6fb0ec8c466c6fbd41b83d56d3cd2
unknown <mvdcreativo@gmail.com>
2020-07-03 00:39:30 -0300

    agrego tag google analitycs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { NgxSpinnerService } from "ngx-spinner";
 import { NavigationEnd, Router } from '@angular/router';
 
 declare let fbq:Function;//facebook pixel
-
+declare let gtag:Function;//facebook pixel
 
 @Component({
   selector: 'app-root',
@@ -23,6 +23,10 @@ export class AppComponent {
       router.events.subscribe((y: NavigationEnd) => {
         if(y instanceof NavigationEnd){
           fbq('track', 'PageView');
+
+          gtag('config', 'G-8VTYKQ2XSH', {
+            'page_path' : y.urlAfterRedirects
+          })
         }
       })
       //////
